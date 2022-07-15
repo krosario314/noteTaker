0f92bc859e74fd324f79e3ccee95020c4040b763
Kayla Rosario <rosariok314@gmail.com>
2022-07-15 15:51:24 -0400

    server js file added

---
+++ server.js
@@ -0,0 +1,19 @@
+const htmlRoutes = require('./routes/htmlRoutes');
+const express = require('express');
+const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const PORT = process.env.PORT || 3003;
+
+app.use(express.static("public"));
+
+// Middleware to JSON 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+});
+
+module.exports = app;
