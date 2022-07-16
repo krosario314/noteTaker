20d8d5db56ed1211fa10f8fdd7184f92daef6a37
Kayla Rosario <rosariok314@gmail.com>
2022-07-15 21:50:52 -0400

    edited server js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
+// adding apps needed for 
 const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 const path = require('path');
-
 const express = require('express');
 const app = express();
-
 const allNotes = require('./db/db.json');
 
 app.use(express.urlencoded({ extended: true }));
